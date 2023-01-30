827f4742f973169c719f90c4f80b374c0b79cc96
RangerUFO <ufownl@gmail.com>
2023-01-30 22:15:23 +0800

    Adapt to the latest release of Gym

--- environment.py
+++ environment.py
@@ -3,21 +3,19 @@ from tqdm import tqdm
 
 
 def run(agent, episodes, before_episode=None, after_episode=None, **kwargs):
-    env = gym.make(agent.environment)
+    env = gym.make(agent.environment, render_mode="human") if agent.test else gym.make(agent.environment)
     agent.spaces = (env.observation_space, env.action_space)
     for i in tqdm(range(episodes)):
         if not before_episode is None:
             before_episode(i + 1)
         g = agent(**kwargs)
         next(g)
-        state = env.reset()
+        state, _ = env.reset()
         reward = 0
         while True:
-            if agent.test:
-                env.render()
             action = g.send((state, reward))
-            state, reward, done, _ = env.step(action)
-            if done:
+            state, reward, terminated, truncated, _ = env.step(action)
+            if terminated or truncated:
                 try:
                     g.send((None, reward))
                 except StopIteration:
